6baaeeaf3ed70c4f9d6c6a629431174629ab5f35
Josh Guyette <joshguyette@yahoo.com>
2026-09-05 10:18:19 -0500

    release: denext 2.0.1

--- mod.ts
+++ mod.ts
@@ -330,4 +330,4 @@ export type {
 export { isPublicEnvKey, PUBLIC_ENV_PREFIXES, publicEnv } from "./src/runtime/public-env.ts";
 
 /** The denext framework version. */
-export const VERSION = "2.0.0";
+export const VERSION = "2.0.1";
